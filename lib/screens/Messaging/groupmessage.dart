a46de3c9b9338194a2558f231416387158cdfac8
hadimortaja <hadimortaja9@gmail.com>
2021-02-11 12:12:20 +0200

    Group Messaging

--- lib/screens/Messaging/groupmessage.dart
+++ lib/screens/Messaging/groupmessage.dart
@@ -1,17 +1,122 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:provider/provider.dart';
+import 'package:social_media_app/screens/Homepage/homepage.dart';
+import 'package:social_media_app/screens/Messaging/groupmessagehelpers.dart';
+import 'package:social_media_app/services/authentication.dart';
 
 class GroupMessage extends StatelessWidget {
   final DocumentSnapshot documentSnapshot;
+  TextEditingController messageController = TextEditingController();
+
   GroupMessage({@required this.documentSnapshot});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          Provider.of<Authentication>(context, listen: false).getUserUid ==
+                  documentSnapshot.data()['useruid']
+              ? IconButton(icon: Icon(Icons.more_vert), onPressed: () {})
+              : Container(
+                  width: 0,
+                  height: 0,
+                ),
+          IconButton(icon: Icon(Icons.logout), onPressed: () {}),
+        ],
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios_rounded,
+          ),
+          onPressed: () {
+            Navigator.pushReplacement(
+                context,
+                PageTransition(
+                    child: HomePage(), type: PageTransitionType.leftToRight));
+          },
+        ),
         backgroundColor: Colors.grey[300],
-        centerTitle: true,
-        title: Text(documentSnapshot.data()['roomname']),
+        title: Container(
+          width: MediaQuery.of(context).size.width * 0.5,
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.grey,
+                backgroundImage:
+                    NetworkImage(documentSnapshot.data()['roomavatar']),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      documentSnapshot.data()['roomname'],
+                      style: TextStyle(color: Colors.black, fontSize: 15),
+                    ),
+                    Text(
+                      "2 Members",
+                      style: TextStyle(color: Colors.grey, fontSize: 12),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              AnimatedContainer(
+                height: MediaQuery.of(context).size.height * 0.8,
+                width: MediaQuery.of(context).size.width,
+                duration: Duration(seconds: 1),
+                curve: Curves.bounceIn,
+              ),
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        child: TextField(
+                          controller: messageController,
+                          decoration: InputDecoration(hintText: 'Drop a hi..'),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 10),
+                        width: 50,
+                        child: RaisedButton(
+                          onPressed: () {
+                            if (messageController.text.isNotEmpty) {
+                              Provider.of<GroupMessageHelper>(context,
+                                      listen: false)
+                                  .sendMessage(context, documentSnapshot,
+                                      messageController);
+                              messageController.clear();
+                            }
+                          },
+                          color: Colors.grey,
+                          child: Icon(
+                            Icons.send,
+                            color: Colors.black,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/screens/Messaging/groupmessagehelpers.dart
+++ lib/screens/Messaging/groupmessagehelpers.dart
@@ -1,5 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:social_media_app/services/authentication.dart';
+import 'package:social_media_app/services/firebase_operations.dart';
 
 class GroupMessageHelper with ChangeNotifier{
+
+  sendMessage(BuildContext context,DocumentSnapshot documentSnapshot,TextEditingController messageController){
+    return FirebaseFirestore.instance.collection('chatrooms').doc(
+      documentSnapshot.id,
+      
+    ).collection('messages').add({
+      'message':messageController.text,
+      'time':Timestamp.now(),
+      'useruid':Provider.of<Authentication>(context,listen: false).getUserUid,
+      'username':Provider.of<FirebaseOperations>(context,listen: false).getinitUserName,
+      'userimage':Provider.of<FirebaseOperations>(context,listen: false).getinitUserImage,
+    });
+
+  }
   
 }
